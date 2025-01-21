19a3148ba3a09080772e563ab39fe3097f0ebc00
JOSEMANUEL <manuel.lopez@microsip.com>
2025-01-21 15:51:11 -0600

    ahora es una api

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+from flask import Flask
 import requests
 
 # Hacer una petición a la API de JSONPlaceholder para obtener todos los posts
 response = requests.get('https://jsonplaceholder.typicode.com/posts')
 
-# Verificar si la petición fue exitosa (código de estado 200)
-if response.status_code == 200:
-    # Convertir la respuesta JSON a un objeto Python
-    posts = response.json()
+app = Flask(__name__)
 
-    # Obtener el título del primer post
-    first_post_title = posts[0]['title']
+@app.route('/')
+def hello():
+    # Verificar si la petición fue exitosa (código de estado 200)
+    if response.status_code == 200:
+        # Convertir la respuesta JSON a un objeto Python
+        posts = response.json()
 
-    print(first_post_title)
-else:
-    print(f"Error al obtener los posts: {response.status_code}")
+        # Obtener el título del primer post
+        first_post_title = posts[0]['title']
+
+        return first_post_title
+    else:
+        return f"Error al obtener los posts: {response.status_code}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
